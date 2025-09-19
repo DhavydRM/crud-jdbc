641a7fb2ec82beb1a98a93f7d0e2a5a668ae9b88
DhavydRM <dhavydromano@gmail.com>
2025-09-19 20:43:10 -0300

    Criação de classe DAO para a classe Usuário que será responsável por fazer todas as querys no banco de dados

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,60 @@
+package dao;
+
+import entidade.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDao {
+
+    private Connection conexao;
+    private PreparedStatement ps;
+    private ResultSet resultSet;
+    private Usuario usuario;
+
+    public UsuarioDao(Connection conexao) {
+        this.conexao = conexao;
+    }
+
+    public void criarUsuario(Usuario usuario) {
+
+        try {
+            ps = conexao.prepareStatement("INSERT INTO usuarios (nome, email, senha)" +
+                    "VALUES (?, ?, ?)");
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getEmail());
+            ps.setString(3, usuario.getSenha());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Deu ruim na Query");
+        }
+
+    }
+
+    public List<Usuario> listarUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+
+        try {
+            ps = conexao.prepareStatement("SELECT * FROM usuarios");
+            resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String nome = resultSet.getString(2);
+                String email = resultSet.getString(3);
+                String senha = resultSet.getString(4);
+                usuarios.add(new Usuario(id, nome, email, senha));
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Deu ruim na Query!");
+        }
+
+        return usuarios;
+    }
+}
